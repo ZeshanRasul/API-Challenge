dea5483d45fd305536b566fe306ffff4e4ef15c0
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-23 17:22:29 +0000

    update

--- app/app.rb
+++ app/app.rb
@@ -2,17 +2,25 @@ require 'sinatra/base'
 require_relative './models/message'
 require_relative 'data_mapper_setup'
 require 'dm-validations'
+require 'rest-client'
 
 class MessageBoard < Sinatra::Base
+  use Rack::MethodOverride
 
   get '/' do
     @messages = Message.all
     erb :index
   end
 
-  post '/' do
-    @message = Message.create(content: params[:content])
-    redirect to '/'
+  # post '/' do
+  #   @message = Message.create(content: params[:content])
+  #   redirect to '/'
+  # end
+  #
+  # delete '/' do
+  #   p params
+  #   @message = Message.get(params[:id]).destroy
+  #   redirect '/'
   end
 
 end
