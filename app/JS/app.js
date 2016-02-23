19f79c10b6367d4abc11bceaaa08bc529cc623d2
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-02-23 18:36:36 +0000

    illuminati haters

---
+++ app/JS/app.js
@@ -0,0 +1,17 @@
+
+
+getData = function() {
+  var xmlhttp =
+}
+
+postData = function() {
+  var xmlhttp =
+}
+
+deleteData = function() {
+  var xmlhttp =
+}
+
+updateData = function() {
+  var xmlhttp =
+}
--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,7 @@ require_relative './models/message'
 require_relative 'data_mapper_setup'
 require 'dm-validations'
 require 'rest-client'
+require 'json'
 
 class MessageBoard < Sinatra::Base
   use Rack::MethodOverride
@@ -11,6 +12,29 @@ class MessageBoard < Sinatra::Base
     @messages = Message.all
     erb :index
   end
+
+  post '/json/create' do
+    content_type :json
+    @message = Message.create(content: params[:content])
+    @message.to_json
+    p JSON.generate(request.body)
+    redirect to '/'
+  end
+
+  get '/json/delete' do
+    content_type :json
+    @message = Message.destroy(content: params[:content])
+    @message.to_json
+    redirect to '/'
+  end
+
+  post 'json/update' do
+    content_type :json
+    redirect to '/'
+  end
+
+
+
 
   # post '/' do
   #   @message = Message.create(content: params[:content])
@@ -21,6 +45,5 @@ class MessageBoard < Sinatra::Base
   #   p params
   #   @message = Message.get(params[:id]).destroy
   #   redirect '/'
-  end
 
 end
