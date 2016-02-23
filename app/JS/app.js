ac02e22002e09cf779e3dc11b33efd52213ba758
ZeshanRasul <zeshanrasul90@gmail.com>
2016-02-23 22:03:19 +0000

    Began get data function

--- app/JS/app.js
+++ app/JS/app.js
@@ -1,17 +1,22 @@
 
 
 getData = function() {
-  var xmlhttp =
+  var xmlhttp = new XMLHttpRequest();
+  var url = '/';
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
 }
 
 postData = function() {
+  var xmlhttp = new XMLHttpRequest();
+
+}
+
+
+updateData = function() {
   var xmlhttp =
 }
 
 deleteData = function() {
-  var xmlhttp =
-}
-
-updateData = function() {
   var xmlhttp =
 }
