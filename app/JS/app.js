86763a10f44701831049fbe99dbec511abb17fc4
ZeshanRasul <zeshanrasul90@gmail.com>
2016-02-24 17:51:38 +0000

    Updated app.js, included into erb, updated edit functionality

--- app/JS/app.js
+++ app/JS/app.js
@@ -1,40 +1,43 @@
-
-
-getData = function() {
-  var xmlhttp = new XMLHttpRequest();
-  var url = '/json/create'
-
-postData = function() {
-  var xmlhttp =
-
-}
-
-
-updateData = function() {
-  var xmlhttp =
-}
-
-deleteData = function() {
-  var xmlhttp =
-
-
+//
+//
+// getData = function() {
+//   var xmlhttp = new XMLHttpRequest();
+//   var url = '/json/create';
+// };
+//
+//
+// postData = function() {
+//   var xmlhttp = new XMLHttpRequest();
+//
+// };
+//
+//
+// updateData = function() {
+//   var xmlhttp = XMLHttpRequest();
+// };
+//
+// deleteData = function() {
+//   var xmlhttp =
+//
+//
 function getData() {
   var xmlhttp = new XMLHttpRequest();
   var url = '/json/update';
   alert('edit');
-  p 'updata DAtata'
+  console.log('updata DAtata');
 
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       document.getElementById('each_message').innerHTML = xmlhttp.responseText;
       }
-    }
+    };
     xmlhttp.open("POST", url, true);
-    xmlhttp.send()
-  }
+    xmlhttp.send();
 }
-function editButtonListener(){
+
+
+editButtonListener = function() {
   var edit = document.getElementById('edit_button');
-  edit.addEventListener("click", updateData);
-  console.log('insdie listerenr')
-}
+  edit.addEventListener("click", getData);
+  console.log('insdie listerenr');
+};
---
+++ app/public/js/app.js
@@ -0,0 +1,41 @@
+//
+//
+// getData = function() {
+//   var xmlhttp = new XMLHttpRequest();
+//   var url = '/json/create'
+//
+// postData = function() {
+//   var xmlhttp =
+//
+// }
+//
+//
+// updateData = function() {
+//   var xmlhttp =
+// }
+//
+// deleteData = function() {
+//   var xmlhttp =
+//
+//
+// function getData() {
+//   var xmlhttp = new XMLHttpRequest();
+//   var url = '/json/update';
+//   alert('edit');
+//   p 'updata DAtata'
+//
+//   xmlhttp.onreadystatechange = function() {
+//     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//       document.getElementById('each_message').innerHTML = xmlhttp.responseText;
+//       }
+//     }
+//     xmlhttp.open("POST", url, true);
+//     xmlhttp.send()
+//   }
+// }
+// function editButtonListener(){
+//   var edit = document.getElementById('edit_button');
+//   edit.addEventListener("click", updateData);
+//   console.log('insdie listerenr')
+// }
+console.log('hi')
